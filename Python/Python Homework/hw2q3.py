9a21d083741a329a354f4f8e3b33f1900fdca354
Mlisq <bsibaij@163.com>
2021-01-06 12:46:21 +0800

    update fk shit

--- Python/Python Homework/hw2q3.py
+++ Python/Python Homework/hw2q3.py
@@ -1,10 +1,10 @@
 def partition(a = []):
     point = 0
     for i in range(len(a)):
-        if(a[point] <= a[i]):
+        if(a[point] < a[i]):
             a[point], a[i] = a[i], a[point]
             point = i
-    return point
+    return point+1
 
 def quicksort(a = []):
     if len(a) < 2 :
@@ -18,3 +18,4 @@ val = input("Enter numbers : \n").split(' ')
 a = [int(i) for i in val if i != '']
 a = quicksort(a)
 print(a)
+
