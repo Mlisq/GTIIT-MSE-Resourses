db8be8436e2e28ec19e9e4c1355c498084b21407
Mlisq <bsibaij@163.com>
2021-01-11 13:28:37 +0800

    update

--- Python/Python Homework/hw3q2.py
+++ Python/Python Homework/hw3q2.py
@@ -3,7 +3,7 @@ def f(s):
         return f(s[1:])
     if len(s) < 3:
         return 0
-    if s[0] < s[1] < s[2] or s[0] > s[1] > s[2]:
+    if (s[0] < s[1] and s[1] > s[2]) or (s[0] > s[1] and s[1] < s[2]):
         return 1 + f(s[1:])
     return f(s[1:])
 
