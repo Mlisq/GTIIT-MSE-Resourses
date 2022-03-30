044ef51646c040179c5c21f39304d93b2263e3eb
Mlisq <bsibaij@163.com>
2022-03-30 22:42:04 +0800

    update 30/3

--- Courses/315058 - COMPUTER ANALYSIS METHODS - Spring 2022/Homeworks/hw2.py
+++ Courses/315058 - COMPUTER ANALYSIS METHODS - Spring 2022/Homeworks/hw2.py
@@ -26,7 +26,7 @@ plt.ylabel('y values')
 plt.title('999006125 Yang Bai')
 
 #Part a
-r_xNumbers = np.sort(np.random.uniform(-5,5,11))
+r_xNumbers = np.linspace(-5,5,11)
 r_yNumbers = 1 / (1 + r_xNumbers**2)
 
 #Part b
@@ -38,7 +38,8 @@ l_yNumbers = l_Tangents(r_xNumbers)
 plt.plot(r_xNumbers,l_yNumbers,color='#F45F3F',label = 'Lagrange')
 
 #Part d
-ch_Tangents = itp.CubicSpline(r_xNumbers, r_yNumbers)
+ch_yNumberTangent = (-1) * ((2*r_xNumbers) / (1+r_xNumbers**2)**2)
+ch_Tangents = itp.CubicHermiteSpline(r_xNumbers, r_yNumbers, ch_yNumberTangent)
 ch_yNumbers = ch_Tangents(r_xNumbers)
 plt.plot(r_xNumbers,ch_yNumbers,'--',color='#3F59F4',label = 'Cubic')
 plt.legend()
@@ -53,20 +54,21 @@ plt.subplot(2,1,1)
 r_sxValues = np.linspace(-4*np.pi, 4*np.pi,256)
 r_syValues = np.sin(r_sxValues)
 noise = np.random.uniform(-0.1,0.1,1)
-randomX = int(np.random.uniform(0,256,1))   #Little complex in order to highlight the noise point
+randomX = int(np.random.uniform(0,256,1))   #Highlight the noise point
 noise_x = r_sxValues[randomX]
 r_syValues[randomX] = noise
 plt.plot(r_sxValues,r_syValues,'.',color='#990CF3',label='Original')
 plt.plot(noise_x,noise,'.',color='#F30C63',label='Noise')
 plt.legend()
 
-#Part b
+#Part b 
 plt.subplot(2,1,2)
-#T = np.pi*4/256
+T = np.pi*8/256
 f_yValues = np.fft.fft(r_syValues)
-#f_xValues = np.fft.fftfreq(256, T)
-f_xValues = np.arange(len(r_syValues))
-plt.plot(f_xValues,np.abs(f_yValues),color='#F87305',label='Yang Bai')
+f_xValues = np.fft.fftfreq(256, T)
+print(f_xValues[randomX])
+print(f_yValues[randomX])
+plt.plot(f_xValues*2*np.pi,np.abs(f_yValues),color='#F87305',label='Yang Bai')
 plt.legend()
 plt.show()
 
