510a068a5f943a3f64325b35eaf111b116468632
Mlisq <bsibaij@163.com>
2022-06-28 14:32:38 +0800

    u

--- Courses/315058 - COMPUTER ANALYSIS METHODS - Spring 2022/References/Simpson and fixed order Gauss quadrature.py
+++ Courses/315058 - COMPUTER ANALYSIS METHODS - Spring 2022/References/Simpson and fixed order Gauss quadrature.py
@@ -11,15 +11,17 @@ print("4th order Gaussian : Result:", t[0],"Error :", abs(t[0] - 2*np.arctan(10)
 print("--------------")
 
 #Simpson
-#求椭圆体积 双重积分
-a, b, c = 3, 4, 5
-volume = (4/3)*np.pi*a*b*c
-def f1(y,x):
-    return 2*c*np.sqrt(1 - ((x**2)/a**2) - ((y**2)/b**2))
-def f2(x):
-    return [(-1) * b * np.sqrt(1 - ((x**2)/a**2)), b * np.sqrt(1 - ((x**2)/a**2))]
-nq = ite.nquad(f1, [f2, [-a , a]])
-error = np.abs(nq[0] - volume)
-print("Result from nqaud :",nq[0])
-print("Error is ", error)
-print("---------------------")
+N = 1717
+x = np.linspace(-a, a, N+1)
+def f3(x):
+    return b*np.sqrt(1-((x**2)/a**2))
+ylist = np.zeros(N+1)
+for i in range(len(x)):
+    y = np.linspace(-f3(x[i]), f3(x[i]), int(2 * f3(x[i]) / (2*a/N))+1)
+    z = 2*c*np.sqrt(abs(1-((x[i]**2)/a**2) - ((y**2)/b**2)))
+    ylist[i] = ite.simpson(z,y)
+ss = ite.simpson(ylist,x)
+ss_error = abs(ss - volume)
+print("Result from simpson :",ss)
+print("Error is ", ss_error)
+print("Spacing is ", 2*a/N)
